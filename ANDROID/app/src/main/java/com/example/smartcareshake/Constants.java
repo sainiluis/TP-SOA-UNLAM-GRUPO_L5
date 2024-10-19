36507b00f8990a33c823abca59e2073ee09b3ce8
sainiluis <luis.saini@hotmail.com>
2024-10-18 21:05:20 -0300

    add android shake
    add android shake

---
+++ ANDROID/app/src/main/java/com/example/smartcareshake/Constants.java
@@ -0,0 +1,8 @@
+package com.example.smartcareshake;
+
+public class Constants {
+
+    public static final int TIEMPO_APLAZO = 10000;
+    public static final int COUNTDOWN = 1000;
+    public static final int DELAY_SPLASH = 1000;
+}
---
+++ ANDROID/app/src/main/java/com/example/smartcareshake/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.smartcareshake;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        ImageView splashImage = findViewById(R.id.splash_image);
+        Animation slideLeft = AnimationUtils.loadAnimation(this, R.anim.slide_left);
+        splashImage.startAnimation(slideLeft);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, Constants.DELAY_SPLASH);
+    }
+}
