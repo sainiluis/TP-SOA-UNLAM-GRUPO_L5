0d112547510c143b400d1ed27550cad4084ff5bd
sainiluis <luis.saini@hotmail.com>
2024-10-28 20:17:12 -0300

    luis

--- ANDROID/app/src/main/java/com/example/smartcareshake/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.smartcareshake;
-
-public class Constants {
-
-    public static final int TIEMPO_APLAZO = 10000;
-    public static final int COUNTDOWN = 1000;
-    public static final int DELAY_SPLASH = 1000;
-}
